4ff4de90032b121bb58daf5b8a97d2052624a16f
Cristian Gonzalez <cristian.gonzalez@chartboost.com>
2019-11-14 23:26:37 +0100

    Refactoring the users controllers and the user model

--- users_api/controller/users.py
+++ users_api/controller/users.py
@@ -6,6 +6,7 @@ from flask import Blueprint, jsonify, request
 from users_api.model.user import User
 from users_api.model.budget import Budget
 from users_api.database import users_database, budget_database
+from users_api.exception.user import NameTooShort, PasswordTooShort, RoleNotValid, EmailNotValid
 
 users_blueprint = Blueprint('users', __name__, url_prefix='/users')
 
@@ -18,41 +19,32 @@ def list_users():
 def create_user():
     request_info = request.get_json()
 
-    name = request_info['name']
-
-    if (len(name) < 5):
-        return "Name cannot be shorter than 5 characters", 400
-
-    password = request_info['password']
-
-    if (len(password) < 8):
-        return "Password cannot be shorter than 8 characters", 400
-
     email = request_info['email']
 
     if (users_database.select_by('email', email)):
         return "User already exists", 400
-
-    if not re.search(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
-        return "Email must be valid", 400
 
     country = request_info['country']
 
     if not pycountry.countries.get(alpha_2=country):
         return "Country must be an ISO-3166 valid", 400
 
-    role = request_info['role']
-
-    if not role in ['advertiser', 'publisher']:
-        return "Role must be 'advertiser' or 'publisher'", 400
-
-    user = User()
-    user.id = str(uuid.uuid4())
-    user.name = name
-    user.email = email
-    user.password = password
-    user.role = role
-    user.country = country
+    try:
+        user = User(
+            request_info['name'],
+            email,
+            request_info['password'],
+            request_info['role'],
+            country
+        )
+    except NameTooShort as ex:
+        return ex.message, 400
+    except PasswordTooShort as ex:
+        return ex.message, 400
+    except EmailNotValid as ex:
+        return ex.message, 400
+    except RoleNotValid as ex:
+        return ex.message, 400
 
     users_database.insert(user.to_dict())
 
--- users_api/model/user.py
+++ users_api/model/user.py
@@ -1,11 +1,37 @@
+import re
+import uuid
+
+from users_api.exception.user import NameTooShort, PasswordTooShort, RoleNotValid, EmailNotValid
+
 class User:
-    def __init__(self):
-        self.id = ''
-        self.name = ''
-        self.country = ''
-        self.email = ''
-        self.password = ''
-        self.role = ''
+    def __init__(self, name, email, password, role, country):
+        self.__assert_name_is_valid(name)
+        self.__assert_password_is_valid(password)
+        self.__assert_email_is_valid(email)
+        self.__assert_role_is_valid(role)
+
+        self.id = str(uuid.uuid4())
+        self.name = name
+        self.country = country
+        self.email = email
+        self.password = password
+        self.role = role
+
+    def __assert_name_is_valid(self, name):
+        if (len(name) < 5):
+            raise NameTooShort()
+
+    def __assert_password_is_valid(self, password):
+        if (len(password) < 8):
+            raise PasswordTooShort()
+
+    def __assert_email_is_valid(self, email):
+        if not re.search(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):            
+            raise EmailNotValid()
+
+    def __assert_role_is_valid(self, role):
+        if not role in ['advertiser', 'publisher']:
+            raise RoleNotValid()
 
     def to_dict(self):
         return {
